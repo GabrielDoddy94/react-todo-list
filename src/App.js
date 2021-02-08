f19ae7738ef0123543d38286772126b93c1f2109
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2021-02-08 02:20:23 -0300

    First Commit:rocket:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const changeInputHandler = e => {
+    setInputText(e.target.value);
+  };
+
+  const addTodoHandler = () => {
+    if (inputText.trim() === '') return;
+
+    setTodos(prevTodo => [...prevTodo, { todo: inputText }]);
+    setInputText('');
+  };
+
+  const deleteTodoHandler = todoIndex => {
+    const todosCopy = [...todos];
+    todosCopy.splice(todoIndex, 1);
+
+    setTodos([...todosCopy]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Todo'>
+      <h3>Todo List </h3>
+
+      <div className='Todo__box--form'>
+        <input
+          className='Todo__input'
+          type='text'
+          onChange={e => changeInputHandler(e)}
+          value={inputText}
+        />
+        <button className='Todo__btn--add' onClick={addTodoHandler}>
+          +
+        </button>
+      </div>
+
+      <ul className='Todo__list'>
+        {todos.map((todo, index) => {
+          return (
+            <li
+              key={index}
+              className='Todo__item'
+              onClick={() => deleteTodoHandler(index)}
+            >
+              {todo.todo} <button className='Todo__btn--delete'>X</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
